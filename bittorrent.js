e973b3c847d02ffc588ce708d1f015d3c2893752
Farid Fadaie <farid.fadaie@gmail.com>
2013-04-05 12:42:44 -0700

    4

--- bittorrent.js
+++ bittorrent.js
@@ -1,2 +1,1 @@
-$('head').append('<link rel="stylesheet" href="https://raw.github.com/faridfadaie/eb_change/master/earbits.css" type="text/css" />');
-alert("Hello, world!");
+$('head').append('<link rel="stylesheet" href="https://raw.github.com/faridfadaie/eb_change/master/test.css" type="text/css" />');
